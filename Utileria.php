f98bcd0e8d6ed2e4503240404a244c23f21ca0ce
NEO <graff_rasmus@hotmail.com>
2017-06-15 21:00:31 -0500

    Agregando la funcion de generateRandomString

--- Utileria.php
+++ Utileria.php
@@ -12,8 +12,18 @@ use yii\helpers\VarDumper;
 
 
 class Utileria {
+
+    public static  function generateRandomString($length = 10) 
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
 	
-	    
     public static function array_combinar($array1,$array2){
 
         $array_final = [];
